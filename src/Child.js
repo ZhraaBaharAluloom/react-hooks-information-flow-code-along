77a49a222fef244a9f2fbaf621fc6625d9d0f83a
ZhraaBaharAluloom <zahraambaharaluloom@gmail.com>
2021-07-28 01:32:47 -0700

    random color

--- src/Child.js
+++ src/Child.js
@@ -1,8 +1,14 @@
 import React from "react";
 import { getRandomColor } from "./randomColorGenerator.js";
 
-function Child(props) {
-  return <div className="child" style={{ backgroundColor: "#FFF" }} />;
+function Child({ handleParentClick, color }) {
+  return (
+    <div
+      className="child"
+      onClick={() => handleParentClick(getRandomColor())}
+      style={{ backgroundColor: color }}
+    ></div>
+  );
 }
 
 export default Child;
--- src/Parent.js
+++ src/Parent.js
@@ -5,11 +5,18 @@ import Child from "./Child";
 function Parent() {
   const randomColor = getRandomColor();
   const [color, setColor] = useState(randomColor);
+  const [childColor, setChildColor] = useState("#FFF");
+
+  const handleParentClick = (random) => {
+    const newRandomColor = getRandomColor();
+    setColor(newRandomColor);
+    setChildColor(random);
+  };
 
   return (
     <div className="parent" style={{ backgroundColor: color }}>
-      <Child />
-      <Child />
+      <Child handleParentClick={handleParentClick} color={childColor} />
+      <Child handleParentClick={handleParentClick} color={childColor} />
     </div>
   );
 }
